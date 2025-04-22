a016b2d33d9b88a37cedbd8dbc6c8e0a3aecb003
KotiYadav <koti0120@gmail.com>
2025-04-22 10:06:42 +0530

    3 tier

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    DB_HOST : 'database-1.c380a08uukyc.ap-south-1.rds.amazonaws.com',
+    DB_HOST : 'database-1.cp2ukiwkoxxg.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
     DB_PWD : 'koti2025',
     DB_DATABASE : 'webappdb'
